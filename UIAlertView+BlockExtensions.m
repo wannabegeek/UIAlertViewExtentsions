b694262b69a8206807cc6b7aae94adf49f44470d
Tom Fewster <tom@wannabegeek.com>
2012-03-17 20:43:22 +0000

    added vargs to add extra buttons to UIAlertView

--- UIAlertView+BlockExtensions.m
+++ UIAlertView+BlockExtensions.m
@@ -9,16 +9,34 @@
 
 @implementation UIAlertView (BlockExtensions)
 
-- (id)initWithTitle:(NSString *)title message:(NSString *)message completionBlock:(void (^)(NSUInteger buttonIndex))block cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ... {
+- (id)initWithTitle:(NSString *)title message:(NSString *)message completionBlock:(void (^)(NSUInteger buttonIndex, UIAlertView *alertView))block cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ... {
 	objc_setAssociatedObject(self, "blockCallback", [block copy], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-	return [self initWithTitle:title message:message delegate:self cancelButtonTitle:cancelButtonTitle otherButtonTitles:otherButtonTitles, nil];
+	
+	if (self = [self initWithTitle:title message:message delegate:self cancelButtonTitle:nil otherButtonTitles:nil]) {
+		
+		if (cancelButtonTitle) {
+			[self addButtonWithTitle:cancelButtonTitle];
+			self.cancelButtonIndex = [self numberOfButtons] - 1;
+		}
+		
+		id eachObject;
+		va_list argumentList;
+		if (otherButtonTitles) {
+			[self addButtonWithTitle:otherButtonTitles];
+			va_start(argumentList, otherButtonTitles);
+			while ((eachObject = va_arg(argumentList, id))) {
+				[self addButtonWithTitle:eachObject];
+			}
+			va_end(argumentList);
+		}
+	}	
+	return self;
 }
 
 
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
-	void (^block)(NSUInteger buttonIndex) = objc_getAssociatedObject(self, "blockCallback");
-	block(buttonIndex);
-//	Block_release(__bridge block);
+	void (^block)(NSUInteger buttonIndex, UIAlertView *alertView) = objc_getAssociatedObject(self, "blockCallback");
+	block(buttonIndex, self);
 }
 
 @end
